a616adcd6130b657824b26ec4461d1a38971a949
Isaac009 <isaactnicholaus@gmail.com>
2024-05-24 22:20:14 +0900

    Add Training and Industry Contents

--- app/Http/Controllers/IndustryController.php
+++ app/Http/Controllers/IndustryController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class IndustryController extends Controller
 {
-    //
+    public function program(){
+        return view('frontend.industry/program');
+    }
+
+    public function engagement(){
+        return view('frontend.industry/engagement');
+    }
 }
